7bf7bf26f4f0863586b4ceeb3854adbcf6f58312
natalka-kar <nataryades@gmail.com>
2022-08-10 21:36:51 +0300

    init: start and finish task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,26 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+        this.middle = 0;
+        this.min = 0;
+        this.max = 0;
+    }
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.middle = Math.ceil((this.min + (this.max - this.min) / 2));
+        return this.middle;
     }
 
     lower() {
-
+        this.max = this.middle;
     }
 
     greater() {
-
+        this.min = this.middle;
     }
-}
 
+}
 module.exports = GuessingGame;
